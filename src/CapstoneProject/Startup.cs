986fc0ee54fc8388fa893129e1836a466c664ace
ian Wilcox <ian Wilcox>
2017-01-04 18:36:24 -0800

    push to github

---
+++ src/CapstoneProject/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+//using CapstoneProject.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+
+namespace CapstoneProject
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; set; }
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath);
+                //.AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            //services.AddEntityFramework()
+            //    .AddDbContext<CapstoneProjectContext>(options =>
+            //        options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+            //services.AddIdentity<ApplicationUser, IdentityRole>()
+            //    .AddEntityFrameworkStores<CapstoneProjectContext>()
+            //    .AddDefaultTokenProviders();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseStaticFiles();
+            loggerFactory.AddConsole();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            //app.UseIdentity();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.Run(async (error) =>
+            {
+                await error.Response.WriteAsync("You should not see this message. An error has occured.");
+            });
+        }
+    }
+}
---
+++ src/CapstoneProject/wwwroot/js/JavaScript.js
@@ -0,0 +1,127 @@
+﻿var curPtsVar = 0
+var plr1PtsTotal = 0
+var plr2PtsTotal = 0
+var whosTurn = 1
+
+function diceRoll() {
+    var rollResult = Math.floor((Math.random() * 6) + 1);
+    return rollResult;
+}
+
+function dicePicture(index) {
+    var string = "dice/dice" + index + ".png";
+    $(".diceImg").attr("src", string);
+}
+
+function postCurrentRoll(index) {
+    $("ul").append("<li>" + index + "</li>");
+}
+
+function totalCurrentPoints(i) {
+    curPtsVar = curPtsVar + i;
+    $(".CurPts").text(curPtsVar);
+}
+
+function rolledOne(i) {
+    if (i === 1) {
+        curPtsVar = 0;
+        $(".CurPts").text(curPtsVar);
+        $("ul").empty();
+        changeTurn();
+    }
+}
+
+
+function sumPts() {
+    if (whosTurn === 1) {
+        plr1PtsTotal = curPtsVar + plr1PtsTotal;
+        $(".plr1").text(plr1PtsTotal);
+    } else if (whosTurn === 2) {
+        plr2PtsTotal = curPtsVar + plr2PtsTotal;
+        $(".plr2").text(plr2PtsTotal);
+    }
+    curPtsVar = 0;
+    $(".CurPts").text(curPtsVar);
+    $("ul").empty();
+}
+
+function changeTurn() {
+    if (whosTurn === 2) {
+        whosTurn = 1;
+    } else {
+        whosTurn = 2;
+    }
+    hideNotTurn()
+}
+
+function hideNotTurn() {
+    if (whosTurn === 2) {
+        $(".turnP1").toggle();
+        $(".turnP2").toggle();
+    } else {
+        $(".turnP2").toggle();
+        $(".turnP1").toggle();
+    }
+}
+
+function winner() {
+    if (plr1PtsTotal > 99) {
+        var name = $("input.player1name").val().toUpperCase();
+
+        $(".gameDisplay").hide();
+        $(".restart").show();
+        $(".restart h1").text(name + " You Are the WINNER!!!!!");
+    }
+    if (plr2PtsTotal > 99) {
+        var name = $("input.player2name").val().toUpperCase();
+
+        $(".gameDisplay").hide();
+        $(".restart").show();
+        $(".restart h1").text(name + " You Are the WINNER!!!!!");
+    }
+}
+
+
+
+
+
+// front end logic
+$(document).ready(function () {
+    $("form#player1name").submit(function (event) {
+        event.preventDefault();
+    });
+
+    $("form#player2name").submit(function (event) {
+        event.preventDefault();
+    });
+
+    $("#ROLLbtn").click(function () {
+        var numRolled = diceRoll();
+        dicePicture(numRolled);
+        postCurrentRoll(numRolled);
+        totalCurrentPoints(numRolled);
+        rolledOne(numRolled);
+        winner();
+    });
+
+    $("#PASSbtn").click(function () {
+        sumPts();
+        changeTurn();
+        winner();
+    });
+
+    $("#ReStartbtn").click(function () {
+        curPtsVar = 0;
+        plr1PtsTotal = 0;
+        plr2PtsTotal = 0;
+        whosTurn = 1;
+        $(".CurPts").text(curPtsVar);
+        $(".plr1").text(plr1PtsTotal);
+        $(".plr2").text(plr1PtsTotal);
+        $(".gameDisplay").show();
+        $(".restart").hide();
+        $(".turnP1").show();
+        $(".turnP2").hide();
+
+    });
+});
